82d8bee2fa99b86eab6e092a51892e201897d07f
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-06 15:46:36 -0500

    Add Command Interface

---
+++ lib/src/main/java/com/marsss/Command.java
@@ -0,0 +1,15 @@
+package com.marsss;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public interface Command {
+    void runCommand(GuildMessageReceivedEvent e);
+    void runSlash(SlashCommandEvent event);
+
+    static String getHelp() {
+        return "Command Help Message Here";
+    }
+
+    String[] getTriggers();
+}
