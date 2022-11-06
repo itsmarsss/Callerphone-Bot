1c60eaaaa9c79a7571d138473dc04b415d551fe0
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-06 16:15:57 -0500

    Add ChannelPooling commands
    Signed-off-by: itsmarsss <itsmarzzzzzz@gmail.com>

---
+++ lib/src/main/java/com/marsss/callerphone/channelpool/JoinPool.java
@@ -0,0 +1,26 @@
+package com.marsss.callerphone.channelpool;
+
+import com.marsss.Command;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class JoinPool implements Command {
+    @Override
+    public void runCommand(GuildMessageReceivedEvent e) {
+
+    }
+
+    @Override
+    public void runSlash(SlashCommandEvent event) {
+
+    }
+
+    public static String getHelp() {
+        return "Help Here";
+    }
+
+    @Override
+    public String[] getTriggers() {
+        return "joinpool".split(",");
+    }
+}
---
+++ lib/src/main/java/com/marsss/callerphone/channelpool/SettingsPool.java
@@ -0,0 +1,26 @@
+package com.marsss.callerphone.channelpool;
+
+import com.marsss.Command;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class SettingsPool implements Command {
+    @Override
+    public void runCommand(GuildMessageReceivedEvent e) {
+
+    }
+
+    @Override
+    public void runSlash(SlashCommandEvent event) {
+
+    }
+
+    public static String getHelp() {
+        return "Help Here";
+    }
+
+    @Override
+    public String[] getTriggers() {
+        return "settingspool,settingpool,setpool".split(",");
+    }
+}
