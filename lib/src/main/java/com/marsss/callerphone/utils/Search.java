2887d0f33eb18b9f1d28154fe3473023860b88c0
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-06 15:13:47 -0500

    Migrate Search cmd to cmdMap

--- lib/src/main/java/com/marsss/callerphone/utils/Search.java
+++ lib/src/main/java/com/marsss/callerphone/utils/Search.java
@@ -3,67 +3,89 @@ package com.marsss.callerphone.utils;
 import java.awt.Color;
 import java.io.IOException;
 
+import com.marsss.Command;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.marsss.callerphone.Callerphone;
-import com.marsss.entertainments.Colour;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
-public class Search {
+public class Search implements Command {
 
-	public static MessageEmbed search(String query) throws IOException {
+    @Override
+    public void runCommand(GuildMessageReceivedEvent e) {
+        final Message MESSAGE = e.getMessage();
+        final String CONTENT = MESSAGE.getContentRaw();
+        if (CONTENT.substring(8).equals("")) {
+            MESSAGE.reply("Please enter a search query!").queue();
+            return;
+        }
 
-		query = query.substring(1, query.length());
-		
-		final String url = "https://www.duckduckgo.com/html" + "?q=" + query;	 
+        try {
+            MESSAGE.replyEmbeds(search(CONTENT.substring(8))).queue();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            MESSAGE.reply("Error getting links").queue();
+        }
+    }
 
-		final Document doc = Jsoup.connect(url).get();
+    public MessageEmbed search(String query) throws IOException {
+        query = query.substring(1);
 
-		final Elements links = doc.getElementById("links").getElementsByClass("results_links");
+        final String url = "https://www.duckduckgo.com/html" + "?q=" + query;
 
-		final Color COLOR = Colour.randColor();
+        final Document doc = Jsoup.connect(url).get();
 
-		EmbedBuilder GglEmd = new EmbedBuilder()
-				.setColor(COLOR)
-				.setTitle("Search Results for *" + query + "*", url.replaceAll("\\s+", "%20"));
+        final Elements links = doc.getElementById("links").getElementsByClass("results_links");
 
-		for(int i = 0; i < 3; i++) {
-			try {
-				final Element currlink = links.get(i).getElementsByClass("links_main").first().getElementsByTag("a").first();
+        final Color COLOR = Colour.randColor();
 
-				String title = currlink.text();
-				String snippet = links.get(i).getElementsByClass("result__snippet").first().text();
-				String hyper = currlink.attr("href");
+        EmbedBuilder GglEmd = new EmbedBuilder()
+                .setColor(COLOR)
+                .setTitle("Search Results for *" + query + "*", url.replaceAll("\\s+", "%20"));
 
-				if(title.length() > 100) {
-					title = title.substring(0, 197) + "...";
-				}
+        for (int i = 0; i < 3; i++) {
+            try {
+                final Element currlink = links.get(i).getElementsByClass("links_main").first().getElementsByTag("a").first();
 
-				if(snippet.length() > 1000) {
-					snippet = snippet.substring(0, 1997) + "...";
-				}
+                String title = currlink.text();
+                String snippet = links.get(i).getElementsByClass("result__snippet").first().text();
+                String hyper = currlink.attr("href");
 
-				GglEmd.addField("__" + title + "__", 
-						snippet + 
-						"\n[[Link]](" + hyper + ")", false);
-			}catch(Exception e) {
-				continue;
-			}
+                if (title.length() > 100) {
+                    title = title.substring(0, 197) + "...";
+                }
 
-		}
+                if (snippet.length() > 1000) {
+                    snippet = snippet.substring(0, 1997) + "...";
+                }
 
-		
-		
-		return GglEmd.build();
-	}
+                GglEmd.addField("__" + title + "__",
+                        snippet +
+                                "\n[[Link]](" + hyper + ")", false);
+            } catch (Exception e) {
+            }
 
-	public static String getHelp() {
-		return "`" + Callerphone.Prefix + "search <query>` - Search for something quickly on the web with title, snippet, and link!";
-	}
+        }
+
+
+        return GglEmd.build();
+    }
+
+    @Override
+    public String getHelp() {
+        return "`" + Callerphone.Prefix + "search <query>` - Search for something quickly on the web with title, snippet, and link!";
+    }
+
+    @Override
+    public String[] getTriggers() {
+        return new String[0];
+    }
 
 }
