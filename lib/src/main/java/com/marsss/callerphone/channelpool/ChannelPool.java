b0e3559b6755971577fd8a407705edb6fb3ac4f8
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-06 16:04:04 -0500

    Fix Channel Pool logic errors
    Signed-off-by: itsmarsss <itsmarzzzzzz@gmail.com>

--- lib/src/main/java/com/marsss/callerphone/channelpool/ChannelPool.java
+++ lib/src/main/java/com/marsss/callerphone/channelpool/ChannelPool.java
@@ -1,12 +1,15 @@
-package com.marsss;
+package com.marsss.callerphone.channelpool;
 
 import com.marsss.callerphone.Callerphone;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ChannelPool {
 
-    public HashMap<String, String>passw = new HashMap<String, String>();
-    public HashMap<String, String>parent = new HashMap<String, String>();
-    public HashMap<String, ArrayList<String>>childr = new HashMap<String, ArrayList<String>>();
+    public HashMap<String, String> passw = new HashMap<>();
+    public HashMap<String, String> parent = new HashMap<>();
+    public HashMap<String, ArrayList<String>> childr = new HashMap<>();
 
     public void setPassword(String ID, String PASS) {
         passw.put(ID, PASS);
@@ -17,28 +20,35 @@ public class ChannelPool {
     }
 
     public void clearChildren(String ID) {
-        ArrayList<String>pool = childr.get(id);
-        for(String id : pool) {
+        ArrayList<String> pool = childr.get(ID);
+        for (String id : pool) {
             Callerphone.jda.getTextChannelById(id).sendMessage("This pool has been ended by the host channel.").queue();
             parent.remove(id);
         }
-        childr.remove(id);
+        childr.remove(ID);
     }
 
     public void addChildren(String IDh, String IDc) {
-        if(childr.get(IDh) != null) {
+        if (childr.get(IDh) != null) {
             childr.get(IDh).add(IDc);
             parent.put(IDc, IDh);
             // TODO:
-                // Joined/Success messages
+            // Joined/Success messages
+        } else {
+            // TODO:
+            // ID not hosted
         }
     }
 
     public void removeChildren(String IDh, String IDc) {
-        if(childr.get(IDh)) == null) {
-            childr.put(IDh, new ArrayList<String>());
+        if(parent.get(IDc) != null) {
+            childr.get(IDh).remove(IDc);
+            parent.remove(IDc);
+            // TODO:
+            // Leave/Success messages
+        } else {
+            // TODO:
+            // Not in pool
         }
-        childr.get(IDh).remove(IDc);
-        parent.remove(IDc);
     }
 }
