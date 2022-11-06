dcd01539af5ee8d42d1ead1b1160cd1fbed8520c
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-06 15:09:31 -0500

    Migrate RoleInfo cmd to cmdMap

--- lib/src/main/java/com/marsss/callerphone/utils/RoleInfo.java
+++ lib/src/main/java/com/marsss/callerphone/utils/RoleInfo.java
@@ -2,73 +2,102 @@ package com.marsss.callerphone.utils;
 
 import java.awt.Color;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
+import com.marsss.Command;
 import com.marsss.callerphone.Callerphone;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-public class RoleInfo {
+public class RoleInfo implements Command {
 
-	public static MessageEmbed roleinfo(Role role) {
-		Color COLOR = role.getColor();
-		String NAME = role.getName();
-		String ID = role.getId();
-		String DATE_CREATED = role.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME);
-		String PERMISSIONS = "";
-		String MEMBERS_WITH_ROLE = "";
-		String POSITION = String.valueOf(role.getGuild().getRoles().size() - role.getPosition());
-		String ISHOISTED = String.valueOf(role.isHoisted());
-		String ISMANAGED = String.valueOf(role.isManaged());
-		String ISMENTIONABLE =	String.valueOf(role.isMentionable());
-		String ISPUBLICROLE = String.valueOf(role.isPublicRole());
+    @Override
+    public void runCommand(GuildMessageReceivedEvent e) {
+        final Message MESSAGE = e.getMessage();
+        final String CONTENT = MESSAGE.getContentRaw();
+        final String args[] = CONTENT.split("\\s+");
+        final List<Role> ROLES = MESSAGE.getMentionedRoles();
+        Role role;
 
-		for (Permission p : role.getPermissions()) {
-			PERMISSIONS += p.getName() + ", ";
-		}
-		if (PERMISSIONS.length() > 0) {
-			PERMISSIONS = PERMISSIONS.substring(0, PERMISSIONS.length()-2);
-		}else
-			PERMISSIONS = "No permissions.";
+        try {
+            role = Callerphone.jda.getRoleById(Long.parseLong(args[1]));
+        }catch(Exception ex) {
+            role = null;
+        }
 
+        if(ROLES.size() > 0) {
+            if(role == null)
+                role = ROLES.get(0);
+        }else if(role == null) {
+            MESSAGE.reply("Please specify a role").queue();
+            return;
+        }
 
-		for (Member m : role.getGuild().getMembersWithRoles(role)) {
-			MEMBERS_WITH_ROLE += m.getAsMention() + ", ";
-		}
-		if (MEMBERS_WITH_ROLE.length() > 0) {
-			MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, MEMBERS_WITH_ROLE.length()-2);
-			if(MEMBERS_WITH_ROLE.length() > 1024) {
-				MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, 1000);
-				MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, MEMBERS_WITH_ROLE.lastIndexOf(","));
-				MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE + "` + " + (role.getGuild().getMembers().size() - (MEMBERS_WITH_ROLE.length() - MEMBERS_WITH_ROLE.replaceAll("@", "").length())) + " more`";
-			}
-		}else
-			MEMBERS_WITH_ROLE = "No member has this Role.";
+        Color COLOR = role.getColor();
+        String NAME = role.getName();
+        String ID = role.getId();
+        String DATE_CREATED = role.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME);
+        String PERMISSIONS = "";
+        String MEMBERS_WITH_ROLE = "";
+        String POSITION = String.valueOf(role.getGuild().getRoles().size() - role.getPosition());
+        String ISHOISTED = String.valueOf(role.isHoisted());
+        String ISMANAGED = String.valueOf(role.isManaged());
+        String ISMENTIONABLE = String.valueOf(role.isMentionable());
+        String ISPUBLICROLE = String.valueOf(role.isPublicRole());
 
-		EmbedBuilder RleInfEmd = new EmbedBuilder()
-				.setColor(COLOR)
-				.setDescription("📝 **Role information for " + role.getAsMention() + ":**")
-				.addField("Name", NAME, false)
-				.addField("Permissions", PERMISSIONS, false)
-				.addField("Members With Role", MEMBERS_WITH_ROLE, false)
-				.addField("Creation Date", DATE_CREATED, false)
-				.addField("Position", POSITION, false)
-				.addField("Apart from online", ISHOISTED, true)
-				.addField("Integration", ISMANAGED, true)
-				.addField(" ", " ", true)
-				.addField("Mentionable", ISMENTIONABLE, true)
-				.addField("Public Role", ISPUBLICROLE, true)
-				.addField(" ", " ", true)
-				.setFooter("ID: " + ID);
+        for (Permission p : role.getPermissions()) {
+            PERMISSIONS += p.getName() + ", ";
+        }
+        if (PERMISSIONS.length() > 0) {
+            PERMISSIONS = PERMISSIONS.substring(0, PERMISSIONS.length() - 2);
+        } else
+            PERMISSIONS = "No permissions.";
 
 
-		return RleInfEmd.build();
-	}
+        for (Member m : role.getGuild().getMembersWithRoles(role)) {
+            MEMBERS_WITH_ROLE += m.getAsMention() + ", ";
+        }
+        if (MEMBERS_WITH_ROLE.length() > 0) {
+            MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, MEMBERS_WITH_ROLE.length() - 2);
+            if (MEMBERS_WITH_ROLE.length() > 1024) {
+                MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, 1000);
+                MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE.substring(0, MEMBERS_WITH_ROLE.lastIndexOf(","));
+                MEMBERS_WITH_ROLE = MEMBERS_WITH_ROLE + "` + " + (role.getGuild().getMembers().size() - (MEMBERS_WITH_ROLE.length() - MEMBERS_WITH_ROLE.replaceAll("@", "").length())) + " more`";
+            }
+        } else
+            MEMBERS_WITH_ROLE = "No member has this Role.";
 
-	public static String getHelp() {
-		return "`" + Callerphone.Prefix + "roleinfo <@role/id>` - Get information about this role.";
-	}
+        EmbedBuilder RleInfEmd = new EmbedBuilder()
+                .setColor(COLOR)
+                .setDescription("📝 **Role information for " + role.getAsMention() + ":**")
+                .addField("Name", NAME, false)
+                .addField("Permissions", PERMISSIONS, false)
+                .addField("Members With Role", MEMBERS_WITH_ROLE, false)
+                .addField("Creation Date", DATE_CREATED, false)
+                .addField("Position", POSITION, false)
+                .addField("Apart from online", ISHOISTED, true)
+                .addField("Integration", ISMANAGED, true)
+                .addField(" ", " ", true)
+                .addField("Mentionable", ISMENTIONABLE, true)
+                .addField("Public Role", ISPUBLICROLE, true)
+                .addField(" ", " ", true)
+                .setFooter("ID: " + ID);
+
+        e.getMessage().replyEmbeds(RleInfEmd.build()).queue();
+    }
+
+    @Override
+    public String getHelp() {
+        return "`" + Callerphone.Prefix + "roleinfo <@role/id>` - Get information about this role.";
+    }
+
+    @Override
+    public String[] getTriggers() {
+        return "roleinfo,rolinfo,roleinf,rolinf".split(",");
+    }
 }
