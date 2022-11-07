c5c7491f31010f351acfb0dc6591e1b497f4768d
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-07 11:35:34 -0500

    Add slash command
    Signed-off-by: itsmarsss <itsmarzzzzzz@gmail.com>

--- lib/src/main/java/com/marsss/callerphone/channelpool/EndPool.java
+++ lib/src/main/java/com/marsss/callerphone/channelpool/EndPool.java
@@ -3,30 +3,19 @@ package com.marsss.callerphone.channelpool;
 import com.marsss.Command;
 import com.marsss.callerphone.Callerphone;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class EndPool implements Command {
     @Override
     public void runCommand(GuildMessageReceivedEvent e) {
-        if (!e.getMember().hasPermission(Permission.MANAGE_CHANNEL)) {
-            e.getMessage().reply("You need `Manage Channel` permission to run this command.").queue();
-            return;
-        }
-
-        final Message MESSAGE = e.getMessage();
-        int stat = ChannelPool.endPool(e.getChannel().getId());
-        if (stat == 404) {
-            MESSAGE.reply(Callerphone.Callerphone + "This channel is not hosting a pool.").queue();
-        } else if (stat == 200) {
-            e.getMessage().reply(Callerphone.Callerphone + "Successfully ended channel pool!").queue();
-        }
+        e.getMessage().reply(endPool(e.getMember(), e.getChannel().getId())).queue();
     }
 
     @Override
-    public void runSlash(SlashCommandEvent event) {
-
+    public void runSlash(SlashCommandEvent e) {
+        e.reply(endPool(e.getMember(), e.getChannel().getId())).queue();
     }
 
     public static String getHelp() {
@@ -41,5 +30,19 @@ public class EndPool implements Command {
     @Override
     public String[] getTriggers() {
         return "end,endpool,stoppool".split(",");
+    }
+
+    private String endPool(Member member, String id) {
+        if (!member.hasPermission(Permission.MANAGE_CHANNEL)) {
+            return "You need `Manage Channel` permission to run this command.";
+        }
+
+        int stat = ChannelPool.endPool(id);
+        if (stat == 404) {
+            return Callerphone.Callerphone + "This channel is not hosting a pool.";
+        } else if (stat == 200) {
+            return Callerphone.Callerphone + "Successfully ended channel pool!";
+        }
+        return Callerphone.Callerphone + "An error occurred.";
     }
 }
