995b6dbf321027be0add09c2400af3544e7509a9
itsmarsss <itsmarzzzzzz@gmail.com>
2022-11-30 23:17:36 -0500

    Add properties command

---
+++ lib/src/main/java/com/marsss/callerphone/channelpool/commands/PoolProperties.java
@@ -0,0 +1,27 @@
+package com.marsss.callerphone.channelpool.commands;
+
+import com.marsss.ICommand;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class PoolProperties implements ICommand {
+    @Override
+    public void runCommand(GuildMessageReceivedEvent e) {
+
+    }
+
+    @Override
+    public void runSlash(SlashCommandEvent e) {
+
+    }
+
+    @Override
+    public String getHelpF() {
+        return null;
+    }
+
+    @Override
+    public String[] getTriggers() {
+        return new String[0];
+    }
+}
