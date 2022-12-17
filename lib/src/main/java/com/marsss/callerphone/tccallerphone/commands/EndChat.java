da314aec1a400b7d3acde739788573b6d31c3f47
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-17 01:01:57 -0500

    Make endchat work

--- lib/src/main/java/com/marsss/callerphone/tccallerphone/commands/EndChat.java
+++ lib/src/main/java/com/marsss/callerphone/tccallerphone/commands/EndChat.java
@@ -1,18 +1,28 @@
 package com.marsss.callerphone.tccallerphone.commands;
 
 import com.marsss.ICommand;
+import com.marsss.callerphone.Callerphone;
+import com.marsss.callerphone.tccallerphone.TCCallerphone;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class EndChat implements ICommand {
     @Override
     public void runCommand(GuildMessageReceivedEvent e) {
-
+        e.getMessage().reply(endChat(e.getChannel())).queue();
     }
 
     @Override
     public void runSlash(SlashCommandEvent e) {
 
+    }
+
+    private String endChat(TextChannel channel) {
+        if(!TCCallerphone.hasCall(channel.getId())) {
+            return Callerphone.Callerphone + "There is not a call going on!";
+        }
+        return TCCallerphone.onEndCallCommand(channel);
     }
 
     @Override
