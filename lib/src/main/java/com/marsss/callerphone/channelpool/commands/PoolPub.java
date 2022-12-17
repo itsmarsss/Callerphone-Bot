9757e7998793dc805786edd360d01c75d32ef0f6
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-17 01:03:25 -0500

    Use pool enum

--- lib/src/main/java/com/marsss/callerphone/channelpool/commands/PoolPub.java
+++ lib/src/main/java/com/marsss/callerphone/channelpool/commands/PoolPub.java
@@ -3,6 +3,7 @@ package com.marsss.callerphone.channelpool.commands;
 import com.marsss.ICommand;
 import com.marsss.callerphone.Callerphone;
 import com.marsss.callerphone.channelpool.ChannelPool;
+import com.marsss.callerphone.channelpool.PoolStatus;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -38,10 +39,10 @@ public class PoolPub implements ICommand {
     }
 
     private String poolPub(String id, boolean pub) {
-        int stat = ChannelPool.setPublicity(id, pub);
-        if (stat == ChannelPool.SUCCESS) {
+        PoolStatus stat = ChannelPool.setPublicity(id, pub);
+        if (stat == PoolStatus.SUCCESS) {
             return Callerphone.Callerphone + "This pool is now **" + (pub ? "public" : "private") + "**.";
-        } else if (stat == ChannelPool.ERROR) {
+        } else if (stat == PoolStatus.ERROR) {
             return Callerphone.Callerphone + "This pool is not hosting a pool.";
         }
         return Callerphone.Callerphone + "An error occurred.";
