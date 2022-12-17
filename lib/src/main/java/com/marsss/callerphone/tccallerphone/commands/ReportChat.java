0cc6f17a7c86747f93cfe4ce971228c8dfacafe3
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-17 01:03:47 -0500

    Make reportchat work

--- lib/src/main/java/com/marsss/callerphone/tccallerphone/commands/ReportChat.java
+++ lib/src/main/java/com/marsss/callerphone/tccallerphone/commands/ReportChat.java
@@ -1,18 +1,34 @@
 package com.marsss.callerphone.tccallerphone.commands;
 
 import com.marsss.ICommand;
+import com.marsss.callerphone.Callerphone;
+import com.marsss.callerphone.tccallerphone.ConvoStorage;
+import com.marsss.callerphone.tccallerphone.TCCallerphone;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class ReportChat implements ICommand {
     @Override
     public void runCommand(GuildMessageReceivedEvent e) {
-
+        e.getMessage().reply(reportChat(e.getChannel())).queue();
     }
 
     @Override
     public void runSlash(SlashCommandEvent e) {
 
+    }
+
+    private String reportChat(TextChannel channel) {
+        if (!TCCallerphone.hasCall(channel.getId())) {
+            return Callerphone.Callerphone + "There isn't a chat going on!";
+        }
+        ConvoStorage call = TCCallerphone.getCall(channel.getId());
+        if (call != null) {
+            call.setReport(true);
+            return Callerphone.Callerphone + "Chat reported!";
+        }
+        return Callerphone.Callerphone + "Something went wrong, couldn't report call.";
     }
 
     @Override
