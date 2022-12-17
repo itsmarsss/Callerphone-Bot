e2beb64b16ef8a0270a9f85bd5740d51ad51b2de
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-17 01:03:18 -0500

    Use pool enum

--- lib/src/main/java/com/marsss/callerphone/channelpool/commands/PoolCap.java
+++ lib/src/main/java/com/marsss/callerphone/channelpool/commands/PoolCap.java
@@ -3,6 +3,7 @@ package com.marsss.callerphone.channelpool.commands;
 import com.marsss.ICommand;
 import com.marsss.callerphone.Callerphone;
 import com.marsss.callerphone.channelpool.ChannelPool;
+import com.marsss.callerphone.channelpool.PoolStatus;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -38,10 +39,10 @@ public class PoolCap implements ICommand {
     }
 
     private String poolCap(String id, int cap) {
-        int stat = ChannelPool.setCap(id, cap);
-        if (stat == ChannelPool.SUCCESS) {
+        PoolStatus stat = ChannelPool.setCap(id, cap);
+        if (stat == PoolStatus.SUCCESS) {
             return Callerphone.Callerphone + "This pool now has capacity **" + ChannelPool.config.get(id).getCap() + "**.";
-        } else if (stat == ChannelPool.ERROR) {
+        } else if (stat == PoolStatus.ERROR) {
             return Callerphone.Callerphone + "This channel is not hosing a pool.";
         }
         return Callerphone.Callerphone + "An error occurred.";
