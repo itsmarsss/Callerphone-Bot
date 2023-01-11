cf04fd32c87979ad26fd75cc90719c62278b7406
itsmarsss <itsmarzzzzzz@gmail.com>
2023-01-11 18:48:44 -0500

    Add initial message check to slash commands

--- lib/src/main/java/com/marsss/callerphone/listeners/OnSlashCommand.java
+++ lib/src/main/java/com/marsss/callerphone/listeners/OnSlashCommand.java
@@ -2,6 +2,7 @@ package com.marsss.callerphone.listeners;
 
 import com.marsss.callerphone.Callerphone;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +12,17 @@ public class OnSlashCommand extends ListenerAdapter {
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         try {
             if (Callerphone.cmdMap.containsKey(event.getName())) {
+                if(Callerphone.getCredits(event.getUser()) == 0) {
+                    event.replyEmbeds(
+                            new EmbedBuilder()
+                                    .setTitle("User Agreement", event.getUser().getAvatarUrl())
+                                    .setDescription("By issuing another Callerphone (**\"Bot\"**) command, it is expected that you (**\"User\"**) have read, and User has agreed to both Bot's [Privacy Policy](" + Callerphone.privacy + ") and [Terms of Service](" + Callerphone.terms + "). It is User's responsibility to regularly check for updates to these documents.")
+                                    .setFooter("This is to protect both Bot and User from unforeseen issues in the future. Please read these documents carefully.", Callerphone.jda.getSelfUser().getAvatarUrl())
+                                    .build()
+                    ).queue();
+                    return;
+                }
+
                 Callerphone.cmdMap.get(event.getName()).runSlash(event);
                 Callerphone.reward(event.getUser(), 3);
                 Callerphone.addExecute(event.getUser(), 1);
