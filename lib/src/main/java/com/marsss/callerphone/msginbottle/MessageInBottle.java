8899240b95be0988fad685410a9ce280da3b8e28
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-21 20:51:07 -0400

    Add MIB control

---
+++ lib/src/main/java/com/marsss/callerphone/msginbottle/MessageInBottle.java
@@ -0,0 +1,41 @@
+package com.marsss.callerphone.msginbottle;
+
+import java.util.LinkedList;
+
+public class MessageInBottle {
+    public MIBStatus sendBottle() {
+
+        return MIBStatus.NOT_FOUND;
+    }
+
+    public MIBBottle findBottle() {
+
+        return null;
+    }
+
+    public MIBStatus deleteBottle() {
+
+        return MIBStatus.NOT_FOUND;
+    }
+
+    public LinkedList<MIBBottle> getSent() {
+
+        return null;
+    }
+
+    public LinkedList<MIBBottle> getFound() {
+
+        return null;
+    }
+
+    public LinkedList<MIBBottle> getStarred() {
+
+        return null;
+    }
+
+    public MIBBottle getBottle() {
+
+        return null;
+    }
+
+}
