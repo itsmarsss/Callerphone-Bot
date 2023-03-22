cc6ab92c6b9be8fffb70fb83ae3c2dc95dfb07ab
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-21 23:23:30 -0400

    Add sendBottle and findBottle

--- lib/src/main/java/com/marsss/callerphone/msginbottle/MessageInBottle.java
+++ lib/src/main/java/com/marsss/callerphone/msginbottle/MessageInBottle.java
@@ -1,41 +1,52 @@
 package com.marsss.callerphone.msginbottle;
 
-import java.util.LinkedList;
+import com.marsss.callerphone.Storage;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MessageInBottle {
-    public MIBStatus sendBottle() {
 
-        return MIBStatus.NOT_FOUND;
+    public static final HashMap<String, MIBBottle> bottles = new HashMap<>();
+
+    public MIBStatus sendBottle(String id, String page) {
+        if(Storage.getMIBSendCoolDown(id)-System.currentTimeMillis() > 86400000) {
+            return MIBStatus.RATE_LIMITED;
+        }
+
+        MIBBottle newBottle = new MIBBottle(id, page);
+
+        bottles.put(newBottle.getUuid(), newBottle);
+
+        return MIBStatus.SENT;
     }
 
-    public MIBBottle findBottle() {
+    public MIBBottle findBottle(String id) {
+        if(Storage.getMIBFindCoolDown(id)-System.currentTimeMillis() > 43200000) {
+            return null;
+        }
 
-        return null;
+        MIBBottle bottle = null;
+        int index = 0;
+
+        Iterator it = bottles.entrySet().iterator();
+        while (it.hasNext()) {
+            MIBBottle tempBottle = (MIBBottle) ((Map.Entry)it.next()).getValue();
+            if(!tempBottle.getParticipantID().getLast().equals(id)) {
+                bottle = tempBottle;
+                bottles.remove(index);
+                break;
+            }
+            index++;
+        }
+
+        return bottle;
     }
 
-    public MIBStatus deleteBottle() {
-
-        return MIBStatus.NOT_FOUND;
-    }
-
-    public LinkedList<MIBBottle> getSent() {
-
-        return null;
-    }
-
-    public LinkedList<MIBBottle> getFound() {
-
-        return null;
-    }
-
-    public LinkedList<MIBBottle> getStarred() {
-
-        return null;
-    }
-
-    public MIBBottle getBottle() {
-
-        return null;
+    public MIBBottle getBottle(String uuid) {
+        return bottles.get(uuid);
     }
 
 }
