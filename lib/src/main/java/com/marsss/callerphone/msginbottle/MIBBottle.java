459c2574f16c4b97c173e5bcc70918f6dbdc49a6
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-21 23:22:44 -0400

    Add getters and setters

--- lib/src/main/java/com/marsss/callerphone/msginbottle/MIBBottle.java
+++ lib/src/main/java/com/marsss/callerphone/msginbottle/MIBBottle.java
@@ -1,13 +1,15 @@
 package com.marsss.callerphone.msginbottle;
 
 import java.util.LinkedList;
+import java.util.UUID;
 
 public class MIBBottle {
-    private String UUID;
+    private String uuid;
     private LinkedList<String> participantID;
     private LinkedList<String> page;
 
     public MIBBottle(String participantID, String page) {
+        uuid = UUID.randomUUID().toString();
         addParticipant(participantID);
         addPage(page);
     }
@@ -18,5 +20,29 @@ public class MIBBottle {
 
     public void addPage(String page) {
         this.page.add(page);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public LinkedList<String> getParticipantID() {
+        return participantID;
+    }
+
+    public void getParticipantID(LinkedList<String> participantID) {
+        this.participantID = participantID;
+    }
+
+    public LinkedList<String> getPage() {
+        return page;
+    }
+
+    public void getPage(LinkedList<String> page) {
+        this.page = page;
     }
 }
