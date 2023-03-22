c7c222eb4d702169c26b8e77b7e030ba7b750b9c
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-21 21:30:23 -0400

    Set up mib bottle

--- lib/src/main/java/com/marsss/callerphone/msginbottle/MIBBottle.java
+++ lib/src/main/java/com/marsss/callerphone/msginbottle/MIBBottle.java
@@ -1,4 +1,22 @@
 package com.marsss.callerphone.msginbottle;
 
+import java.util.LinkedList;
+
 public class MIBBottle {
+    private String UUID;
+    private LinkedList<String> participantID;
+    private LinkedList<String> page;
+
+    public MIBBottle(String participantID, String page) {
+        addParticipant(participantID);
+        addPage(page);
+    }
+
+    public void addParticipant(String participantID) {
+        this.participantID.add(participantID);
+    }
+
+    public void addPage(String page) {
+        this.page.add(page);
+    }
 }
