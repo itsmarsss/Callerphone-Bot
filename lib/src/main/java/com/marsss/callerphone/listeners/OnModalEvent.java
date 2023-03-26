f217ef75d7a6c3ef9af2f8402dbc91bb63562431
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-26 13:55:47 -0400

    Add modal listener

---
+++ lib/src/main/java/com/marsss/callerphone/listeners/OnModalEvent.java
@@ -0,0 +1,12 @@
+package com.marsss.callerphone.listeners;
+
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class OnModalEvent extends ListenerAdapter {
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent e) {
+
+    }
+}
