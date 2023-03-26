c1bcda8c2dde918a4ab9591680983e2fe9842764
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-26 13:36:45 -0400

    Implement send bottle

--- lib/src/main/java/com/marsss/callerphone/msginbottle/commands/SendBottle.java
+++ lib/src/main/java/com/marsss/callerphone/msginbottle/commands/SendBottle.java
@@ -1,18 +1,36 @@
 package com.marsss.callerphone.msginbottle.commands;
 
+import com.marsss.callerphone.Callerphone;
+import com.marsss.callerphone.ToolSet;
+import com.marsss.callerphone.msginbottle.MIBStatus;
+import com.marsss.callerphone.msginbottle.MessageInBottle;
 import com.marsss.commandType.ISlashCommand;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class SendBottle implements ISlashCommand {
     @Override
     public void runSlash(SlashCommandInteractionEvent e) {
+        User user = e.getUser();
+        String message = e.getOption("message").getAsString();
+        boolean anonymous = e.getOption("anonymous").getAsBoolean();
 
+        MIBStatus stat = MessageInBottle.sendBottle(user.getId(), message, anonymous);
+
+        switch(stat) {
+            case RATE_LIMITED:
+                e.reply(ToolSet.CP_ERR + "You have reached your max send limit.").setEphemeral(true).queue();
+                break;
+            case SENT:
+                e.reply(ToolSet.CP_EMJ + "Your bottle has been sent!").setEphemeral(true).queue();
+                break;
+        }
     }
 
 
     @Override
     public String getHelp() {
-        return null;
+        return "`" + Callerphone.config.getPrefix() + "sendbottle` - Send a message in bottle into the seas for someone to read.";
     }
 
     @Override
