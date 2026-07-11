90decbf5aaca57803ca104dcfdc8d29fd20ff050
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2026-07-11 08:02:23 -0400

    fix(minigames): ttt buttons, diagonal win, handler

--- app/src/main/java/com/itsmarsss/callerphone/minigames/games/TicTacToe.java
+++ app/src/main/java/com/itsmarsss/callerphone/minigames/games/TicTacToe.java
@@ -1,8 +1,10 @@
 package com.itsmarsss.callerphone.minigames.games;
 
 import com.itsmarsss.callerphone.Callerphone;
+import com.itsmarsss.callerphone.Constants;
 import com.itsmarsss.callerphone.minigames.IMiniGame;
 import com.itsmarsss.callerphone.minigames.MiniGameStatus;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.components.actionrow.ActionRow;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
@@ -10,22 +12,25 @@ import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class TicTacToe implements IMiniGame {
+    private static final String X = "\u274E";
+    private static final String O = "\uD83C\uDD7E";
+    private static final String EMPTY = "\u2B1B";
+
     private String fromChannelID;
     private String toChannelID;
-
     private String fromMessageID;
     private String toMessageID;
-
     private String fromUserID;
     private String toUserID;
-
     private String id;
     private int stage;
 
-    private final int[][] tttMap = new int[][]{
+    /** -1 empty, 0 = from player (X), 1 = to player (O) */
+    private final int[][] board = {
             {-1, -1, -1},
             {-1, -1, -1},
             {-1, -1, -1}
@@ -42,53 +47,48 @@ public class TicTacToe implements IMiniGame {
     public void initGame(String fromChannelID, String toChannelID, String fromUserID, String toUserID) {
         this.fromChannelID = fromChannelID;
         this.toChannelID = toChannelID;
-
         this.fromUserID = fromUserID;
         this.toUserID = toUserID;
-
         this.id = UUID.randomUUID().toString().replace("-", "");
+        this.stage = 0;
     }
 
     @Override
     public int checkForWin() {
         int win = checkRows();
-
         if (win != -1) {
             return win;
         }
-
         win = checkColumns();
-
         if (win != -1) {
             return win;
         }
-
         return checkDiagonals();
     }
 
     @Override
     public String getFromUserId() {
-        return this.fromUserID;
+        return fromUserID;
     }
 
     @Override
     public String getToUserId() {
-        return this.toUserID;
+        return toUserID;
     }
 
     @Override
     public String getID() {
-        return this.id;
+        return id;
     }
 
     @Override
     public String getToMessageId() {
-        return this.toMessageID;
+        return toMessageID;
     }
 
     @Override
     public String getFromMessageId() {
-        return this.fromMessageID;
+        return fromMessageID;
     }
 
     @Override
@@ -103,12 +103,12 @@ public class TicTacToe implements IMiniGame {
 
     @Override
     public String getToChannelId() {
-        return this.toChannelID;
+        return toChannelID;
     }
 
     @Override
     public String getFromChannelId() {
-        return this.fromChannelID;
+        return fromChannelID;
     }
 
     @Override
@@ -122,36 +122,33 @@ public class TicTacToe implements IMiniGame {
     }
 
     public void incrementStage() {
-        this.stage++;
+        stage++;
     }
 
     public int getStage() {
-        return this.stage;
+        return stage;
     }
 
     public MiniGameStatus fromMove(int r, int c) {
-        if (tttMap[r][c] == -1) {
-            tttMap[r][c] = 0;
-            return MiniGameStatus.SUCCESS_MOVE;
-        }
-        return MiniGameStatus.INVALID_MOVE;
+        return place(r, c, 0);
     }
 
     public MiniGameStatus toMove(int r, int c) {
-        if (tttMap[r][c] == -1) {
-            tttMap[r][c] = 1;
-            return MiniGameStatus.SUCCESS_MOVE;
+        return place(r, c, 1);
+    }
+
+    private MiniGameStatus place(int r, int c, int player) {
+        if (r < 0 || r > 2 || c < 0 || c > 2 || board[r][c] != -1) {
+            return MiniGameStatus.INVALID_MOVE;
         }
-        return MiniGameStatus.INVALID_MOVE;
+        board[r][c] = player;
+        return MiniGameStatus.SUCCESS_MOVE;
     }
 
     private int checkRows() {
         for (int i = 0; i < 3; i++) {
-            if (tttMap[i][0] != -1 &&
-                    tttMap[i][0] == tttMap[i][1] &&
-                    tttMap[i][1] == tttMap[i][2]) {
-
-                return tttMap[i][0];
+            if (board[i][0] != -1 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0];
             }
         }
         return -1;
@@ -159,172 +156,89 @@ public class TicTacToe implements IMiniGame {
 
     private int checkColumns() {
         for (int i = 0; i < 3; i++) {
-            if (tttMap[0][i] != -1 &&
-                    tttMap[0][i] == tttMap[1][i] &&
-                    tttMap[1][i] == tttMap[2][i]) {
-
-                return tttMap[0][i];
+            if (board[0][i] != -1 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i];
             }
         }
         return -1;
     }
 
     private int checkDiagonals() {
-        if (tttMap[0][0] != -1 &&
-                tttMap[0][0] == tttMap[1][1] &&
-                tttMap[1][1] == tttMap[2][2]) {
-
-            return tttMap[0][0];
+        if (board[0][0] != -1 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return board[0][0];
         }
-
-        if (tttMap[0][2] != -1 &&
-                tttMap[0][2] == tttMap[1][1] &&
-                tttMap[1][1] == tttMap[2][0]) {
-
-            return tttMap[0][0];
+        if (board[0][2] != -1 && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return board[0][2];
         }
-
         return -1;
     }
 
     public MessageCreateData getMessageForFrom() {
-        MessageCreateBuilder message = new MessageCreateBuilder();
-
-        int win = checkForWin();
-
-        if (win != -1) {
-            if (win == 0) {
-                return getBoardWithMessage("@" + Callerphone.sdMgr.getUserById(fromUserID).getName() + " has won this game!");
-            } else if (win == 1) {
-                return getBoardWithMessage("@" + Callerphone.sdMgr.getUserById(toUserID).getName() + " has won this game!");
-            }
-        } else {
-            message.setContent("@" + Callerphone.sdMgr.getUserById(toUserID).getName() + " has challenged you to a game of TicTacToe");
-        }
-
-        if (this.stage == 9) {
-            return getBoardWithMessage("Tie game!");
-        }
-
-        message.setComponents(getBoard("from"));
-
-        return message.build();
+        return buildTurnMessage("from", toUserID, "has challenged you to a game of TicTacToe");
     }
 
     public MessageCreateData getMessageForTo() {
-        MessageCreateBuilder message = new MessageCreateBuilder();
+        return buildTurnMessage("to", fromUserID, "has challenged you to a game of TicTacToe");
+    }
 
+    private MessageCreateData buildTurnMessage(String side, String challengerId, String challengeText) {
         int win = checkForWin();
-
         if (win != -1) {
-            if (win == 0) {
-                return getBoardWithMessage("@" + Callerphone.sdMgr.getUserById(fromUserID).getName() + " has won this game!");
-
-            } else if (win == 1) {
-                return getBoardWithMessage("@" + Callerphone.sdMgr.getUserById(toUserID).getName() + " has won this game!");
-            }
-        } else {
-            message.setContent("@" + Callerphone.sdMgr.getUserById(fromUserID).getName() + " has challenged you to a game of TicTacToe");
+            return getBoardWithMessage("@" + resolveName(win == 0 ? fromUserID : toUserID) + " has won this game!");
         }
-
-        if (this.stage == 9) {
+        if (stage >= Constants.TICTACTOE_MAX_STAGE) {
             return getBoardWithMessage("Tie game!");
         }
 
-        message.setComponents(getBoard("to"));
-
-        return message.build();
+        return new MessageCreateBuilder()
+                .setContent("@" + resolveName(challengerId) + " " + challengeText)
+                .setComponents(getBoard(side))
+                .build();
     }
 
-    public Collection<ActionRow> getBoard(String prefix) {
-        Collection<ActionRow> collection = new ArrayList<>();
-        Collection<Button> collection1 = new ArrayList<>();
-        Collection<Button> collection2 = new ArrayList<>();
-        Collection<Button> collection3 = new ArrayList<>();
-        for (int c = 0; c < 3; c++) {
-
-            if (tttMap[0][c] == 0) {
-                collection1.add(Button.secondary("invalid1" + UUID.randomUUID(), "\u274E").asDisabled());
-            } else if (tttMap[0][c] == 1) {
-                collection1.add(Button.secondary("invalid1" + UUID.randomUUID(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection1.add(Button.secondary(prefix + "-" + this.id + "-" + "0" + "-" + c + "-" + this.stage, "\u2B1B"));
-            }
-
-            if (tttMap[1][c] == 0) {
-                collection2.add(Button.secondary("invalid2" + UUID.randomUUID(), "\u274E").asDisabled());
-            } else if (tttMap[1][c] == 1) {
-                collection2.add(Button.secondary("invalid2" + UUID.randomUUID(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection2.add(Button.secondary(prefix + "-" + this.id + "-" + "1" + "-" + c + "-" + this.stage, "\u2B1B"));
-            }
-
-            if (tttMap[2][c] == 0) {
-                collection3.add(Button.secondary("invalid3" + UUID.randomUUID(), "\u274E").asDisabled());
-            } else if (tttMap[2][c] == 1) {
-                collection3.add(Button.secondary("invalid3" + UUID.randomUUID(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection3.add(Button.secondary(prefix + "-" + this.id + "-" + "2" + "-" + c + "-" + this.stage, "\u2B1B"));
-            }
-
+    private String resolveName(String userId) {
+        if (Callerphone.sdMgr == null || userId == null) {
+            return "Unknown";
         }
+        User user = Callerphone.sdMgr.getUserById(userId);
+        return user != null ? user.getName() : "Unknown";
+    }
 
-        ActionRow row1 = ActionRow.of(collection1);
-        collection.add(row1);
-        ActionRow row2 = ActionRow.of(collection2);
-        collection.add(row2);
-        ActionRow row3 = ActionRow.of(collection3);
-        collection.add(row3);
-
-        return collection;
+    public Collection<ActionRow> getBoard(String side) {
+        return buildBoard(side, false);
     }
 
     public MessageCreateData getBoardWithMessage(String msg) {
-        MessageCreateBuilder message = new MessageCreateBuilder();
+        return new MessageCreateBuilder()
+                .setContent(msg)
+                .setComponents(buildBoard(null, true))
+                .build();
+    }
 
-        message.setContent(msg);
-
-        Collection<ActionRow> collection = new ArrayList<>();
-        Collection<Button> collection1 = new ArrayList<>();
-        Collection<Button> collection2 = new ArrayList<>();
-        Collection<Button> collection3 = new ArrayList<>();
-        for (int c = 0; c < 3; c++) {
-
-            if (tttMap[0][c] == 0) {
-                collection1.add(Button.secondary(UUID.randomUUID().toString(), "\u274E").asDisabled());
-            } else if (tttMap[0][c] == 1) {
-                collection1.add(Button.secondary(UUID.randomUUID().toString(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection1.add(Button.secondary(UUID.randomUUID().toString(), "\u2B1B").asDisabled());
+    private Collection<ActionRow> buildBoard(String side, boolean allDisabled) {
+        List<ActionRow> rows = new ArrayList<>(3);
+        for (int r = 0; r < 3; r++) {
+            List<Button> buttons = new ArrayList<>(3);
+            for (int c = 0; c < 3; c++) {
+                buttons.add(cellButton(r, c, side, allDisabled));
             }
-
-            if (tttMap[1][c] == 0) {
-                collection2.add(Button.secondary(UUID.randomUUID().toString(), "\u274E").asDisabled());
-            } else if (tttMap[1][c] == 1) {
-                collection2.add(Button.secondary(UUID.randomUUID().toString(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection2.add(Button.secondary(UUID.randomUUID().toString(), "\u2B1B").asDisabled());
-            }
-
-            if (tttMap[2][c] == 0) {
-                collection3.add(Button.secondary(UUID.randomUUID().toString(), "\u274E").asDisabled());
-            } else if (tttMap[2][c] == 1) {
-                collection3.add(Button.secondary(UUID.randomUUID().toString(), "\uD83C\uDD7E").asDisabled());
-            } else {
-                collection3.add(Button.secondary(UUID.randomUUID().toString(), "\u2B1B").asDisabled());
-            }
-
+            rows.add(ActionRow.of(buttons));
         }
+        return rows;
+    }
 
-        ActionRow row1 = ActionRow.of(collection1);
-        collection.add(row1);
-        ActionRow row2 = ActionRow.of(collection2);
-        collection.add(row2);
-        ActionRow row3 = ActionRow.of(collection3);
-        collection.add(row3);
-
-        message.setComponents(collection);
-
-        return message.build();
+    private Button cellButton(int r, int c, String side, boolean forceDisabled) {
+        int cell = board[r][c];
+        if (cell == 0) {
+            return Button.secondary("ttt-x-" + UUID.randomUUID(), X).asDisabled();
+        }
+        if (cell == 1) {
+            return Button.secondary("ttt-o-" + UUID.randomUUID(), O).asDisabled();
+        }
+        if (forceDisabled || side == null) {
+            return Button.secondary("ttt-e-" + UUID.randomUUID(), EMPTY).asDisabled();
+        }
+        // ttt-{side}-{gameId}-{row}-{col}
+        return Button.secondary("ttt-" + side + "-" + id + "-" + r + "-" + c, EMPTY);
     }
 }
--- app/src/main/java/com/itsmarsss/callerphone/minigames/handlers/TicTacToeHandler.java
+++ app/src/main/java/com/itsmarsss/callerphone/minigames/handlers/TicTacToeHandler.java
@@ -3,106 +3,152 @@ package com.itsmarsss.callerphone.minigames.handlers;
 import com.itsmarsss.callerphone.Callerphone;
 import com.itsmarsss.callerphone.minigames.MiniGameStatus;
 import com.itsmarsss.callerphone.minigames.games.TicTacToe;
+import com.itsmarsss.callerphone.users.BotUser;
 import com.itsmarsss.commandType.IButtonInteraction;
 import com.itsmarsss.database.categories.Users;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.exceptions.ErrorHandler;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 import net.dv8tion.jda.api.requests.ErrorResponse;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import net.dv8tion.jda.api.utils.messages.MessageEditData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class TicTacToeHandler implements IButtonInteraction {
+    private static final Logger logger = LoggerFactory.getLogger(TicTacToeHandler.class);
+
     @Override
     public void runClick(ButtonInteraction e) {
-        String[] param = e.getButton().getCustomId().replaceFirst("ttt-", "").split("-");
-
-
-        TicTacToe game = (TicTacToe) Users.getUser(e.getUser().getId()).getGame(param[1]);
-
-        if(game == null) {
-            e.reply("Game not found.").setEphemeral(true).queue();
-            e.getMessage().delete().queue();
+        String customId = e.getButton().getCustomId();
+        if (customId == null || !customId.startsWith("ttt-")) {
+            e.reply("Invalid game button.").setEphemeral(true).queue();
             return;
         }
 
-        if (param[0].equals("to")) {
-            MiniGameStatus stat = game.toMove(Integer.parseInt(param[2]), Integer.parseInt(param[3]));
+        String[] param = customId.substring("ttt-".length()).split("-");
+        if (param.length < 4) {
+            e.reply("Invalid game button.").setEphemeral(true).queue();
+            return;
+        }
 
-            if(stat == MiniGameStatus.INVALID_MOVE) {
+        BotUser botUser = Users.getUser(e.getUser().getId());
+        if (botUser == null) {
+            e.reply("User session not found.").setEphemeral(true).queue();
+            return;
+        }
+
+        TicTacToe game = (TicTacToe) botUser.getGame(param[1]);
+        if (game == null) {
+            e.reply("Game not found.").setEphemeral(true).queue();
+            e.getMessage().delete().queue(null, err -> {
+            });
+            return;
+        }
+
+        int row;
+        int col;
+        try {
+            row = Integer.parseInt(param[2]);
+            col = Integer.parseInt(param[3]);
+        } catch (NumberFormatException ex) {
+            e.reply("Invalid move coordinates.").setEphemeral(true).queue();
+            return;
+        }
+
+        String side = param[0];
+        MiniGameStatus stat;
+        if ("to".equals(side)) {
+            stat = game.toMove(row, col);
+            if (stat == MiniGameStatus.INVALID_MOVE) {
                 e.reply("Invalid move.").setEphemeral(true).queue();
                 return;
             }
-
-            MessageChannel channel = Callerphone.sdMgr.getPrivateChannelById(game.getFromChannelId());
-
-            channel.retrieveMessageById(game.getFromMessageId()).queue((message) -> {
-                message.editMessage(MessageEditData.fromCreateData(game.getMessageForFrom())).queue();
-                channel.sendMessage("You've got a game!").queue((msg) -> {
-                    msg.delete().queueAfter(1, TimeUnit.SECONDS);
-                });
-            }, new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, (err) -> {
-                channel.sendMessage(game.getMessageForFrom()).queue();
-                channel.sendMessage("You've got a game!").queue((msg) -> {
-                    msg.delete().queueAfter(1, TimeUnit.SECONDS);
-                });
-            }));
-        } else if (param[0].equals("from")) {
-            MiniGameStatus stat = game.fromMove(Integer.parseInt(param[2]), Integer.parseInt(param[3]));
-
-            if(stat == MiniGameStatus.INVALID_MOVE) {
+            notifyOpponent(game.getFromChannelId(), game.getFromMessageId(), game.getMessageForFrom());
+        } else if ("from".equals(side)) {
+            stat = game.fromMove(row, col);
+            if (stat == MiniGameStatus.INVALID_MOVE) {
                 e.reply("Invalid move.").setEphemeral(true).queue();
                 return;
             }
-
-            MessageChannel channel = Callerphone.sdMgr.getPrivateChannelById(game.getToChannelId());
-
-            channel.retrieveMessageById(game.getToMessageId()).queue((message) -> {
-                message.editMessage(MessageEditData.fromCreateData(game.getMessageForTo())).queue();
-                channel.sendMessage("You've got a game!").queue((msg) -> {
-                    msg.delete().queueAfter(1, TimeUnit.SECONDS);
-                });
-            }, new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, (err) -> {
-                channel.sendMessage(game.getMessageForTo()).queue();
-                channel.sendMessage("You've got a game!").queue((msg) -> {
-                    msg.delete().queueAfter(1, TimeUnit.SECONDS);
-                });
-            }));
+            notifyOpponent(game.getToChannelId(), game.getToMessageId(), game.getMessageForTo());
+        } else {
+            e.reply("Invalid game button.").setEphemeral(true).queue();
+            return;
         }
 
         int win = game.checkForWin();
-
         if (win != -1) {
             if (win == 0) {
-                MessageChannel channel = Callerphone.sdMgr.getPrivateChannelById(game.getFromChannelId());
-
-                channel.retrieveMessageById(game.getFromMessageId()).queue((message) -> {
-                    message.editMessage(MessageEditData.fromCreateData(game.getMessageForFrom())).queue();
-                }, new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, (err) -> {
-                    channel.sendMessage(game.getMessageForFrom()).queue();
-                }));
+                updateBoard(game.getFromChannelId(), game.getFromMessageId(), game.getMessageForFrom());
             } else if (win == 1) {
-                MessageChannel channel = Callerphone.sdMgr.getPrivateChannelById(game.getToChannelId());
-
-                channel.retrieveMessageById(game.getToMessageId()).queue((message) -> {
-                    message.editMessage(MessageEditData.fromCreateData(game.getMessageForTo())).queue();
-                }, new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, (err) -> {
-                    channel.sendMessage(game.getMessageForTo()).queue();
-                }));
+                updateBoard(game.getToChannelId(), game.getToMessageId(), game.getMessageForTo());
             }
-            Users.getUser(game.getFromUserId()).removeGame(game.getID());
-            Users.getUser(game.getToUserId()).removeGame(game.getID());
+            endGame(game);
         }
 
         game.incrementStage();
 
-        if(game.getStage() == 9) {
+        if (game.getStage() == 9) {
             e.getMessage().editMessage(MessageEditData.fromCreateData(game.getBoardWithMessage("Tie Game!"))).queue();
-            Users.getUser(game.getFromUserId()).removeGame(game.getID());
-            Users.getUser(game.getToUserId()).removeGame(game.getID());
-        }else{
+            endGame(game);
+        } else {
             e.getMessage().editMessage(MessageEditData.fromCreateData(game.getBoardWithMessage("Game Sent!"))).queue();
+        }
+    }
+
+    private void notifyOpponent(String channelId, String messageId, MessageCreateData board) {
+        MessageChannel channel = getPrivateChannel(channelId);
+        if (channel == null) {
+            return;
+        }
+
+        updateBoard(channel, messageId, board);
+        channel.sendMessage("You've got a game!").queue(msg ->
+                msg.delete().queueAfter(1, TimeUnit.SECONDS, null, err -> {
+                }));
+    }
+
+    private void updateBoard(String channelId, String messageId, MessageCreateData board) {
+        MessageChannel channel = getPrivateChannel(channelId);
+        if (channel != null) {
+            updateBoard(channel, messageId, board);
+        }
+    }
+
+    private void updateBoard(MessageChannel channel, String messageId, MessageCreateData board) {
+        if (messageId == null) {
+            channel.sendMessage(board).queue();
+            return;
+        }
+        channel.retrieveMessageById(messageId).queue(
+                message -> message.editMessage(MessageEditData.fromCreateData(board)).queue(),
+                new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, err -> channel.sendMessage(board).queue())
+        );
+    }
+
+    private MessageChannel getPrivateChannel(String channelId) {
+        if (channelId == null || Callerphone.sdMgr == null) {
+            return null;
+        }
+        try {
+            return Callerphone.sdMgr.getPrivateChannelById(channelId);
+        } catch (Exception e) {
+            logger.debug("Could not resolve private channel {}", channelId, e);
+            return null;
+        }
+    }
+
+    private void endGame(TicTacToe game) {
+        BotUser from = Users.getUser(game.getFromUserId());
+        BotUser to = Users.getUser(game.getToUserId());
+        if (from != null) {
+            from.removeGame(game.getID());
+        }
+        if (to != null) {
+            to.removeGame(game.getID());
         }
     }
 
