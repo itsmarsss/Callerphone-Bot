fc91291f6de748c02f6b2206f6ee73dbe3fb33eb
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2026-07-11 07:48:27 -0400

    refactor: simplify slash command dispatch path

--- app/src/main/java/com/itsmarsss/callerphone/listeners/OnSlashCommand.java
+++ app/src/main/java/com/itsmarsss/callerphone/listeners/OnSlashCommand.java
@@ -14,47 +14,49 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Random;
 
 public class OnSlashCommand extends ListenerAdapter {
+    private static final Random RANDOM = new Random();
 
+    @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         try {
-            if (Callerphone.cmdMap.containsKey(event.getName())) {
-                if (!Users.hasUser(event.getUser().getId())) {
-                    ToolSet.sendPPAndTOS(event);
-                    return;
-                }
-
-                if (Users.isBlacklisted(event.getUser().getId())) {
-                    event.reply(String.format(Response.BLACKLISTED.toString(), Users.getReason(event.getUser().getId()))).setEphemeral(true).queue();
-                    return;
-                }
-
-                if (System.currentTimeMillis() - Cooldown.getCmdCooldown(event.getUser().getId()) < ToolSet.COMMAND_COOLDOWN) {
-                    ToolSet.sendCommandCooldown(event);
-                    return;
-                }
-
-                Cooldown.setCmdCooldown(event.getUser().getId());
-
-                Users.reward(event.getUser().getId(), 3);
-                Users.addExecute(event.getUser().getId(), 1);
-
-                ((ISlashCommand) Callerphone.cmdMap.get(event.getName())).runSlash(event);
-
-                Random random = new Random();
-                int randomNumber = random.nextInt(7) + 1;
-
-                if(randomNumber == 1) {
-                    event.getChannel().sendMessageEmbeds(Advertisement.generateAd()).queue();
-                }
+            ISlashCommand command = (ISlashCommand) Callerphone.cmdMap.get(event.getName());
+            if (command == null) {
+                event.reply(
+                        ToolSet.CP_EMJ
+                                + "Hmmm, the slash command `"
+                                + event.getName()
+                                + "` shouldn't exist! Please join our support server and report this issue. "
+                                + Callerphone.config.getSupportServer()
+                ).queue();
                 return;
             }
-            event.reply(
-                    ToolSet.CP_EMJ
-                            + "Hmmm, the slash command `"
-                            + event.getName()
-                            + "` shouldn't exist! Please join our support server and report this issue. "
-                            + Callerphone.config.getSupportServer()
-            ).queue();
+
+            String userId = event.getUser().getId();
+            if (!Users.hasUser(userId)) {
+                ToolSet.sendPPAndTOS(event);
+                return;
+            }
+
+            if (Users.isBlacklisted(userId)) {
+                event.reply(String.format(Response.BLACKLISTED.toString(), Users.getReason(userId)))
+                        .setEphemeral(true).queue();
+                return;
+            }
+
+            if (System.currentTimeMillis() - Cooldown.getCmdCooldown(userId) < ToolSet.COMMAND_COOLDOWN) {
+                ToolSet.sendCommandCooldown(event);
+                return;
+            }
+
+            Cooldown.setCmdCooldown(userId);
+            Users.reward(userId, 3);
+            Users.addExecute(userId, 1);
+
+            command.runSlash(event);
+
+            if (RANDOM.nextInt(7) + 1 == 1) {
+                event.getChannel().sendMessageEmbeds(Advertisement.generateAd()).queue();
+            }
         } catch (Exception e) {
             ErrorHandler.handleSlashCommandError(event, e);
         }
